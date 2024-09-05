041e702b56857e7166ae54063651cfd4a24be051
Abdessamed Dallaa <at.dallaa@esi-sba.dz>
2024-09-05 01:03:04 +0000

    refactored code, added services, .env file

--- src/load_data.py
+++ src/load_data.py
@@ -1,79 +1,104 @@
+import os
+from dotenv import find_dotenv, load_dotenv
 import psycopg2
 from neo4j import GraphDatabase
 
-
-# TODO 1 Read data from postgres
-
-conn = psycopg2.connect(
-    database="testdb",
-    user="postgres",
-    password="postgresssssss",
-    host="127.0.0.1",
-    port="5432",
-)
-
-conn.autocommit = True
-
-cursor = conn.cursor()
-
-cursor.execute("""SELECT * FROM tripdata LIMIT 10""")
-trips = [
-    {
-        "vendor_id": trip[0],
-        "pickup_datetime": trip[1],
-        "dropoff_datetime": trip[2],
-        "passenger_count": trip[3],
-        "trip_distance": trip[4],
-        "rate_code_id": trip[5],
-        "store_and_fwd_flag": trip[6],
-        "pickup_location_id": trip[7],
-        "dropoff_location_id": trip[8],
-        "payment_type": trip[9],
-        "fare_amount": trip[10],
-        "extra": trip[11],
-        "mta_tax": trip[12],
-        "tip_amount": trip[13],
-        "tolls_amount": trip[14],
-        "improvement_surcharge": trip[15],
-        "total_amount": trip[16],
-        "congestion_surcharge": trip[17],
-    }
-    for trip in cursor
-]
-
-print(trips)
-
-# TODO 2 Create a corresponding node on Neo4J
-
-URI = "bolt://localhost:7687"
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
 
 
-def create_nodes(transaction, data):
-    props = [
-        ("{" + ", ".join([f'{key}: "{value}"' for key, value in data[i].items()]) + "}")
-        for i in range(len(data))
-    ]
-    queries = "\n".join([f"CREATE (:Trip {prop})" for prop in props])
+class PostgresService:
 
-    transaction.run(queries)
+    def __init__(self, database, user, host="127.0.0.1", port="5432"):
+        PSQL_PASSWORD = os.getenv("PSQL_PASSWORD")
+        conn = psycopg2.connect(
+            database=database,
+            user=user,
+            password=PSQL_PASSWORD,
+            host=host,
+            port=port,
+        )
+        conn.autocommit = True
+        self.conn = conn
+
+    def __call__(self):
+        cursor = self.conn.cursor()
+        return cursor
+
+    def columns(self, table: str):
+        cursor = self()
+        cursor.execute(
+            """
+            SELECT column_name FROM information_schema.columns WHERE table_schema = 'testdb'
+            """
+        )
+        print(cursor.fetchall())
+        cursor.execute(f"SELECT * FROM {table} LIMIT 0")
+        columns = [desc[0] for desc in cursor.description]
+        return columns
+
+    def run_query(self, query: str):
+        cursor = self()
+        cursor.execute(query)
+        return cursor
+
+    def run_queries(self, queries: list[str]):
+        cursor = self()
+        result = []
+        for query in queries:
+            cursor.execute(query)
+            result.append(cursor)
+
+        return result
+
+    def close(self):
+        self.conn.close()
 
 
-def retrieve_nodes(transaction):
-    result = transaction.run("MATCH (t: Trip) RETURN t")
-    return [record for record in result]
+class Neo4JService:
+    def __init__(self, uri):
+        NEO4J_AUTH = os.getenv("NEO4J_AUTH")
+        self.driver = GraphDatabase.driver(uri, auth=NEO4J_AUTH)
+
+    def __call__(self, database):
+        session = self.driver.session(database=database)
+        transaction = session.begin_transaction()
+        return transaction
+
+    def nodes(self, table, limit=25):
+        query = f"MATCH (n:{table}) RETURN n LIMIT {limit}"
+        result = self()
+        result.run(query)
+        return [record for record in result]
+
+    def create(self, table, data):
+        props = [
+            (
+                "{"
+                + ", ".join([f'{key}: "{value}"' for key,
+                            value in item.items()])
+                + "}"
+            )
+            for item in data
+        ]
+        queries = "\n".join([f"CREATE (:{table} {prop})" for prop in props])
+
+        result = self()
+        result.run(queries)
+
+    def delete_table(self, table):
+        query = f"MATCH (t: {table}) DELETE t"
+        result = self()
+        result.run(query)
+
+    def delete_all(self):
+        query = f"MATCH (n) DETACH DELETE n"
+        result = self()
+        result.run(query)
+
+    def close(self):
+        self.driver.close()
 
 
-def delete_nodes(transaction):
-    query = "MATCH (t: Trip) DETACH DELETE t"
-    transaction.run(query)
-
-
-with GraphDatabase.driver(URI, auth=("neo4j", "15012004")) as driver:
-    with driver.session(database="neo4j") as session:
-        with session.begin_transaction() as transaction:
-            create_nodes(transaction, trips)
-            # delete_nodes(transaction)
-            names = retrieve_nodes(transaction)
-            # for name in names:
-            #     print(name)
-            # print((len(names)))
+if __name__ == '__main__':
+    ...
