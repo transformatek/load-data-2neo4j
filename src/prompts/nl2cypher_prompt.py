540ac356016d23fb4569ea3760f347caea13019e
Abdessamed Dallaa <at.dallaa@esi-sba.dz>
2024-09-20 22:53:53 +0000

    incorporated examples into nl to cypher prompt

--- src/prompts/nl2cypher_prompt.py
+++ src/prompts/nl2cypher_prompt.py
@@ -1,12 +1,13 @@
 from services.ai_model_service import AIModelService
 from services.neo4j_service import Neo4JService
+from services.dynamic_example import DynamicExample
 
 
 class NL2CypherPrompt:
 
     def __init__(self):
         self.ai_model_service = AIModelService()
-
+        self.dynamic_example = DynamicExample()
         try:
             self.neo4j_service = Neo4JService()
 
@@ -17,6 +18,7 @@ class NL2CypherPrompt:
 
     def gen_prompt(self, user_input) -> str:
 
+        examples = self.dynamic_example.get_examples(user_input)
         context = self.database_schema
 
         task = f"""You are required to generate Cypher queries that answer the following question(s) 
@@ -25,37 +27,27 @@ class NL2CypherPrompt:
         '{user_input}'
         """
 
-        constraints = """
+        constraints = f"""
         Make sure that the Cypher query does run successfully on the given schema.
         Make sure that your answer is logical.
         Follow the structure of the database schema strictly.
         Do not include any explanations, apologies, or anything other than the Cypher query in your answer.
         """
-        return f"""Context:
-            {context} 
+
+        if len(examples) > 0:
+            questions = [example["input"] for example in examples]
+            queries = [example["query"] for example in examples]
+
+            examples_ = f"""
             
-            Task:
-            {task}
+            Examples:
+            Here are a few examples on some similar user inputs and the appropriate Cypher queries that answer them:
             
-            Constraints:
-            {constraints}
-            
-            Answer:
+            {'\n\n'.join([f"Input: {question}\nQuery: {query}" for question, query in zip(questions, queries)])}
             """
+            constraints += examples_
 
-    def gen_summary_prompt(self) -> str:
-
-        context = self.neo4j_service.nodes(limit=100)
-
-        task = f"""
-        You are required to generate a textual summary of the data provided in the Neo4j database:
-        """
-
-        constraints = """
-        Do not include any explanations, apologies, recommendations, or anything other than the summary of the data in your response.
-        """
-        return f"""
-            Context:
+        return f"""Context:
             {context} 
             
             Task:
