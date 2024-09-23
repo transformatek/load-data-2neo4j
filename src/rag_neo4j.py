0fd96a3048d6ec366c0480c1fd6ba9b6b7fd803b
Abdessamed Dallaa <at.dallaa@esi-sba.dz>
2024-09-23 09:45:19 +0000

    added graph summary file

--- src/rag_neo4j.py
+++ src/rag_neo4j.py
@@ -1,13 +1,10 @@
 from prompts.nl2cypher_prompt import NL2CypherPrompt
-from services.neo4j_service import Neo4JService
-from services.ai_model_service import AIModelService
+
 if __name__ == "__main__":
     print("[RAG NEO4J] Start")
     nl_2_cypher = NL2CypherPrompt()
     user_input = """What cities are located in Australia?"""
     prompt = nl_2_cypher.gen_prompt(user_input)
-    neo4j_service = Neo4JService()
-    ai_model_service = AIModelService()
-    summary_prompt = nl_2_cypher.gen_summary_prompt()
-    print(nl_2_cypher.prompt_llm(summary_prompt))
+    query = nl_2_cypher.prompt_llm(prompt)
+    print(query)
     print("[RAG NEO4J] End")
