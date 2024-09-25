3dc9b5031bfe80ea46ad74ad1f476e78651f7416
Abdessamed Dallaa <at.dallaa@esi-sba.dz>
2024-09-25 21:48:53 +0000

    added qdrant service

---
+++ src/services/qdrant_service.py
@@ -0,0 +1,49 @@
+from qdrant_client import QdrantClient
+from qdrant_client.models import VectorParams, Distance, PointStruct
+import numpy as np
+
+
+class QdrantService:
+    def __init__(self):
+        self.client = QdrantClient(url="http://vdb:6333")
+
+    def __call__(self):
+        return self.client
+
+    def create_collection(self, collection_name: str):
+        if not self().collection_exists(collection_name):
+            self().create_collection(
+                collection_name=collection_name,
+                vectors_config=VectorParams(size=384, distance=Distance.COSINE)
+            )
+
+    def insert_vectors(self, collection_name: str, vectors, payloads):
+        self().upsert(
+            collection_name=collection_name,
+            points=[
+                PointStruct(
+                    id=idx,
+                    vector=vector[0],
+                    payload=vector[1]
+                )
+                for idx, vector in enumerate(zip(vectors, payloads))
+            ]
+        )
+
+    def clear_collection(self, collection_name: str):
+        points = self.client.scroll(
+            collection_name=collection_name, limit=1000)
+
+        ids = [point.id for point in points]
+
+        if len(ids) > 0:
+            self.client.delete(
+                collection_name=collection_name, points=ids)
+
+    def search(self, collection_name, target_vector, top_k=5):
+        hits = self().search(
+            collection_name=collection_name,
+            query_vector=target_vector,
+            limits=top_k
+        )
+        return hits
